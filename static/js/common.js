a8667389e4f45c22c3585eff92ea8e97de323b06
eodog99 <eodog@naver.com>
2025-06-09 16:57:12 +0900

    first commit

---
+++ static/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+   $("#header").load("layout/header.html", function () {
+   });
+
+   $("#nav").load("layout/menu.html", function () {
+  //    initNavScript();
+  });
+
+   $("#footer").load("layout/footer.html", function () {
+   });
+})
+
---
+++ static/js/include.js
@@ -0,0 +1,31 @@
+document.write(`
+    
+    <!-- ibsheet css -->
+    <link rel="stylesheet" href="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/css/style.css" />
+    <link rel="stylesheet"
+        href="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/css/default/main.css" />
+
+    <!-- ibsheet 필수 js -->
+    <script src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/ibleaders.js"></script>
+    <script src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/ibsheet.js"></script>
+    <script src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/locale/ko.js"></script>
+
+    <!-- ibsheet 플러그인 -->
+    <script
+        src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/plugins/ibsheet-common.js"></script>
+    <script
+        src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/plugins/ibsheet-dialog.js"></script>
+    <script
+        src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/ibsheet/plugins/ibsheet-excel.js"></script>
+
+    <!-- 외부 라이브러리 -->
+    <script
+        src="https://demo.ibsheet.com/ibsheet/v8/samples/customer-sample/assets/vendors/fontawesome.min.js"></script>
+
+
+     <!-- jQuery-->
+    <script src="/static/js/jquery-3.6.0.js"></script>
+
+    <!-- common-->
+    <script src="/static/js/common.js"></script>
+    `)
